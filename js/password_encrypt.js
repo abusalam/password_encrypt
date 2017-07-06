6b68be6a6b351e856f73213d30ef9fcb20381a8d
Amit Sharma <sharma.amitt16@gmail.com>
2017-07-06 14:17:31 +0530

    Coding standards changes

--- js/password_encrypt.js
+++ js/password_encrypt.js
@@ -2,13 +2,12 @@
   'use strict';
 	// This function is strict.
   Drupal.behaviors.password_encrypt = {
-	  attach: function (context, settings) {
-		  var passkey = Drupal.settings.password_encrypt.password_encrypt_passkey;
-		  var cipher;
+    attach: function (context, settings) {
+      var passkey = Drupal.settings.password_encrypt.password_encrypt_passkey;
+      var cipher;
       var pass;
-		  var cpass;
+      var cpass;
 
-		  // Login Form.
       $('#user-login').submit(function (event) {
         pass = $('#edit-pass').val();
         if (pass !== '') {
@@ -17,20 +16,19 @@
         }
       });
 			
-			// User registration/profile form.
       $('#user-register-form, #user-profile-form').submit(function(event) {
         pass = $('#edit-pass-pass1').val();
         cpass = $('#edit-pass-pass2').val();
-				
-  			if (pass !== cpass) {
+
+        if (pass !== cpass) {
           $('span.error').append("<div>Password doesn't match. Please enter correct password.<div>");
           $('#edit-pass-pass2').addClass('error').focus();
           return false;
         }
-				
-        if (pass !== '') { 
+
+        if (pass !== '') {
           cipher = CryptoJS.AES.encrypt(JSON.stringify(pass), passkey, {format: CryptoJSAesJson}).toString();	  
-          $('#edit-pass-pass1').val(cipher);					  
+          $('#edit-pass-pass1').val(cipher);
           $('#edit-pass-pass2').val(cipher);
   			}
   		});
