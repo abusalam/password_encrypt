cfc562724ad515caf4110a1090db0530a795c22f
Abu Salam (Aorus-Pro-WiFi) <abusalamparvezalam@gmail.com>
2020-03-16 21:08:16 +0530

    Fixed #3071501 Current Password Is passing in Plane text

--- js/password_encrypt.js
+++ js/password_encrypt.js
@@ -7,6 +7,7 @@
       var cipher;
       var pass;
       var cpass;
+      var current_pass;
 
       $('#user-login, #user-login-form').submit(function (event) {
         pass = $('#edit-pass').val();
@@ -17,6 +18,7 @@
       });
 
       $('#user-register-form, #user-form').submit(function (event) {
+        current_pass = $('#edit-current-pass').val();
         pass = $('#edit-pass-pass1').val();
         cpass = $('#edit-pass-pass2').val();
 
@@ -26,11 +28,17 @@
           return false;
         }
 
+        if (current_pass !== '') {
+          cipher = CryptoJS.AES.encrypt(current_pass, passkey).toString();
+          $('#edit-current-pass').val(cipher);
+        }
+
         if (pass !== '') {
           cipher = CryptoJS.AES.encrypt(pass, passkey);
           $('#edit-pass-pass1').val(cipher);
           $('#edit-pass-pass2').val(cipher);
         }
+
       });
     }
   };
